3c380cfbb4f7d00cce944d5653ddb007ba891c6b
kingcode <samuelvilorio_15@hotmail.com>
2021-12-08 15:55:44 -0400

    feat: now shows the posts and deletes posts by id,

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,37 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 //Routes
-router.get('/', (req, res) =>{
-    res.send("Your first f'ing posts route!!!");
+//get all the posts
+router.get('/', async (req, res) =>{
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch(err){
+        res.json({message: err});
+    }
 });
-router.get('/singlePost', (req, res) =>{
-    res.send("Your first single post!!!");
+
+//get a specific post
+router.get('/:postId', async (req, res) =>{
+    try{
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    }catch(err){
+        res.json({message: err});
+    }
 });
+
+//delete a post
+router.delete('/:postId', async (req, res) =>{
+    try{
+        const removedPost = await Post.remove({_id:req.params.postId});
+        res.json(removedPost);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+//save a post
 router.post('/', async (req, res) =>{
     const post = new Post({
         title: req.body.title,
