50ce04125a2b26f7f2791f4765459aede3bdb7aa
kingcode <samuelvilorio_15@hotmail.com>
2021-12-07 23:54:42 -0400

    feat: now it inserts posts

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require('body-parser');
 require('dotenv/config');
 
 //Middlewares
+app.use(bodyParser.json());
 //app.use('/posts', ()=>{
     //console.log("Your first ever middleware");
 //});
 
 //import routes
 const postsRoute = require('./routes/posts');
+
+//this is a middleware
 app.use('/posts', postsRoute);
 
+//Routes
 app.get('/', (req, res) =>{
     res.send("Your first fucking nodejs route!!!");
 });
 //connect to mongodb
-mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true}, ()=> console.log('Connected'));
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true}, ()=> console.log('Connected'));
+
 // this is the port the server will be listening to
 app.listen(3000);
+
+
+//const uri = "mongodb+srv://m001-student:<password>@restcluster.jev9u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 //Routes
 router.get('/', (req, res) =>{
@@ -7,6 +8,17 @@ router.get('/', (req, res) =>{
 });
 router.get('/singlePost', (req, res) =>{
     res.send("Your first single post!!!");
+});
+router.post('/', async (req, res) =>{
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    try{
+        const savedPost = await post.save();
+    }catch(err){
+        res.json({message: err});
+    }
 });
 //for this to work you got to export the router so that you can use the router module somewhere else in your app
 module.exports = router;
